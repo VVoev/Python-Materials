d70820cd1ef50bbd598f7d6f6317fd14ff098aee
vvoev <vladovoev@yahoo.co.uk>
2017-01-20 15:00:27 +0200

    added python functions

---
+++ Python Functions/1.Functions.py
@@ -0,0 +1,42 @@
+def sumnumbers(a,b):
+    return a+b;
+def givemebiggest(a,b,c):
+    return max(a,b,c);
+def concatstrings(word1,word2):
+    return  word1+" "+word2;
+def printinfo( name, age ):
+   "This prints a passed info into this function"
+   print ("Name: ", name)
+   print ("Age: ", age)
+def main(x,y,z):
+    input_var = input("Enter something: ")
+    print("you entered " + input_var)
+    return ;
+def sayhello(a):
+    print(a+" say hello");
+def Area (a,b):
+    return a*2+b*2;
+
+
+a=sumnumbers(5,25);
+b=givemebiggest(30,40,50);
+c=concatstrings("Kiril","Popov");
+print(a)
+print(b)
+print(c)
+printinfo( age=50, name="miki" )
+sayhello("Petar");
+#main(10,20,30)
+rectangleA= 10;
+rectangleB=20;
+print('Rectangle sides are%s %d with Area: %s' % (rectangleA, rectangleB,Area(rectangleB,rectangleA)))
+
+
+
+
+
+
+
+
+
+
